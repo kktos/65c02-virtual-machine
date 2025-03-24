d64eddac95811215eab019fefafeb324c86f3df1
kktos <tm.barral@gmail.com>
2025-03-24 17:47:36 +0100

    fix: remove asm sources

--- src/debugger/debugger.js
+++ src/debugger/debugger.js
@@ -2,7 +2,6 @@ import { assemble } from "../../assets/libs/libasm6502.js";
 // import { assemble } from "../6502assembler/6502assembler.js";
 import * as utils from "../utils.js";
 import MemViewer from "./mem.js";
-import {window_init} from "./window.js";
 
 // Some attempt at making prevInstruction more accurate; score the sequence of instructions leading
 // up to the target by counting all "common" instructions as a point. The highest-scoring run of
@@ -16,33 +15,35 @@ import {window_init} from "./window.js";
 // const commonInstructions= /(RTS|B..|JMP|JSR|LD[AXY]|ST[AXY]|TA[XY]|T[XY]A|AD[DC]|SUB|SBC|CLC|SEC|CMP|EOR|ORR|AND|INC|DEC).*/;
 // const uncommonInstrucions= /.*,\s*([XY]|X\))$/;
 
-let DISASM_LINES_COUNT= 40;
-
-const id= "65x02 Machine Emulator";
+const id = "65x02 Machine Emulator";
 export default class Debugger {
-
 	constructor(vm, memory) {
-		this.vm= vm;
-		this.memory= new Uint8Array(memory);
+		this.vm = vm;
+		this.memory = new Uint8Array(memory);
+		this.mem= null;
 		// this.disassembler= new Disassembler(vm, memory);
 
-		this.stepCount= Infinity;
-		this.stopOnOpcode= 0;
+		this.stepCount = Number.POSITIVE_INFINITY;
+		this.stopOnOpcode = 0;
+
+		this.lastCPUstate= {};
+		this.disasmLinecount= 0;
+		this.stackLinecount= 0;
 
 		this.setup();
 
-		window.STOP= () => {
-			const cycles_count= vm.waitMessage("stop");
+		window.STOP = () => {
+			const cycles_count = vm.waitMessage("stop");
 			console.log("STOPPED:", cycles_count);
-			vm.isRunning= false;
+			vm.isRunning = false;
 			this.updateBtns(true);
 			this.update();
 		};
 
-		window.DBG= this;
+		window.DBG = this;
 
-		const refresh= () => {
-			this.mem.update();
+		const refresh = () => {
+			this.mem?.update();
 			setTimeout(refresh, 1000);
 		};
 
@@ -50,73 +51,104 @@ export default class Debugger {
 	}
 
 	async setup() {
-		const bps= JSON.parse(localStorage.getItem(`${id}-bps`));
-		this.breakpoints= bps && Array.isArray(bps) ? bps : [];
+		const bps = JSON.parse(localStorage.getItem(`${id}-bps`));
+		this.breakpoints = bps && Array.isArray(bps) ? bps : [];
 
-		await this.vm.waitMessage("initBP",{list: this.breakpoints});
+		await this.vm.waitMessage("initBP", { list: this.breakpoints });
+	}
+
+	addWidget(grid, widget, id) {
+		const el= grid.addWidget(widget);
+		el.id= `${id}Widget`;
+
+		const content= el.querySelector(".grid-stack-item-content");
+		content.appendChild(document.querySelector(`#${id}Tmpl`).content.cloneNode(true));
+		const closeBtn= content.querySelector("#close")
+		closeBtn?.addEventListener("click", (e) => {
+			grid.removeWidget(el);
+			if(widget.onClose) {
+				widget.onClose();
+			}
+		});
+		return content.clientHeight;
 	}
 
 	setupUI() {
-		this.uiroot= document.querySelector("#debugger");
-		this.UIstack= this.uiroot.querySelector("#stack");
-		this.UIdisasm= this.uiroot.querySelector("#disasm");
-		this.UIbps= this.uiroot.querySelector("#bps");
+		this.uiroot = document.querySelector("BODY");
 
-		// setupConsole(this.uiroot.querySelector(".log"));
+		this.grid = document.querySelector('.grid-stack').gridstack;
 
-		this.editValueDlg= this.uiroot.querySelector("#editValueDlg");
-
-		this.diskName= this.uiroot.querySelector("#btns #diskname");
-
-		this.registers= {
-			pc: this.uiroot.querySelector("#registers #PC"),
-			a: this.uiroot.querySelector("#registers #A"),
-			x: this.uiroot.querySelector("#registers #X"),
-			y: this.uiroot.querySelector("#registers #Y"),
-			sp: this.uiroot.querySelector("#registers #SP"),
-			p: this.uiroot.querySelector("#registers #P")
-		}
-
-		DISASM_LINES_COUNT= Math.max(Math.floor(this.UIdisasm.clientHeight/10), 40);
-
-		const memoryPanel= this.uiroot.querySelector("#mem");
-		this.mem= new MemViewer(this, memoryPanel, DISASM_LINES_COUNT);
-
-		this.uiroot.addEventListener("resize", (e)=> {
-			DISASM_LINES_COUNT= Math.max(Math.floor(this.UIdisasm.clientHeight/10), 40);
-			this.mem.resize(DISASM_LINES_COUNT);
+		this.grid.on('change', (_, items) => {
+			const el= items[0].el;
+			const height = el.querySelector(".grid-stack-item-content").clientHeight;
+			switch(el.id) {
+				case "memWidget": {
+					this.mem.resize(Math.floor(height / 11));
+					break;
+				}
+				case "disasmWidget": {
+					this.disasmLinecount = Math.floor(height / 11);
+					this.updateDisasm(this.lastCPUstate);
+					break;
+				}
+				case "stackWidget": {
+					this.stackLinecount = Math.floor(height / 8);
+					this.updateStack(this.lastCPUstate);
+					break;
+				}
+			}
 		});
 
-		this.UIdisasm.addEventListener("click", (e)=> this.onClickDisasm(e));
+		// setupConsole(document.querySelector(".log"));
 
-		this.uiroot
-			.querySelector("#registers")
-			.addEventListener("click", (e)=> this.onClickRegister(e));
+		this.editValueDlg = document.querySelector("#editValueDlg");
 
-		this.UIbps.addEventListener("click", (e)=> this.onClickBreakpoints(e));
+		this.diskName = document.querySelector("#btns #diskname");
 
-		document
-			.querySelectorAll(".btn")
-			.forEach(btn => {
-				btn.addEventListener("click", (e) => this.onClickBtn(e));
-			});
+		let height;
 
-		this.uiroot
-			.querySelectorAll("INPUT")
-			.forEach(btn => {
-				btn.addEventListener("change", (e) => this.onChange(e));
-			});
+		height= this.addWidget(this.grid, {x:7, y:1, w:3, h:5}, "disasm");
+		this.disasmLinecount = Math.floor(height / 11);
+		this.UIdisasm = document.querySelector("#disasm");
+		this.UIdisasm.addEventListener("click", (e) => this.onClickDisasm(e));
 
+		height= this.addWidget(this.grid, {x:10, y:3, w:1, h:4}, "stack");
+		this.stackLinecount = Math.floor(height / 8);
+		this.UIstack = document.querySelector("#stack");
 
-		window_init();
+		this.addWidget(this.grid, {x:10, y:0, w:1, h:1, noResize: true}, "bps");
+		this.UIbps = document.querySelector("#bps");
+
+		this.addWidget(this.grid, {x:10, y:0, w:1, h:1, noResize: true}, "registers");
+		this.registers = {
+			pc: document.querySelector("#registers #PC"),
+			a: document.querySelector("#registers #A"),
+			x: document.querySelector("#registers #X"),
+			y: document.querySelector("#registers #Y"),
+			sp: document.querySelector("#registers #SP"),
+			p: document.querySelector("#registers #P"),
+		};
+		document.querySelector("#registers").addEventListener("click", (e) => this.onClickRegister(e));
+
+		this.addWidget(this.grid, {x:7, y:0, w:3, h:1, noResize: true}, "btns");
+
+		this.UIbps.addEventListener("click", (e) => this.onClickBreakpoints(e));
+
+		for (const btn of document.querySelectorAll(".btn")) {
+			btn.addEventListener("click", (e) => this.onClickBtn(e));
+		}
+
+		for (const btn of document.querySelectorAll("INPUT")) {
+			btn.addEventListener("change", (e) => this.onChange(e));
+		}
 
 		this.updateBtns(false);
 		this.update();
-		this.uiroot.style.visibility= "visible";
+		// this.uiroot.style.visibility = "visible";
 	}
 
 	onChange(e) {
-		switch(e.target.id) {
+		switch (e.target.id) {
 			case "speed":
 				this.vm.setSpeed(e.target.value);
 				break;
@@ -141,18 +173,21 @@ export default class Debugger {
 	// }
 
 	async onClickBtn(e) {
+		switch (e.target.id) {
+			case "close": {
+				console.log(e.target);
+				break;
+			}
 
-		switch(e.target.id) {
 			case "asm-start": {
-				document.querySelector("#asm").style.visibility= "visible";
+				document.querySelector("#asm").style.visibility = "visible";
 				break;
 			}
 
 			case "asm-open": {
-
 				const [fileHandle] = await showOpenFilePicker();
 				const file = await fileHandle.getFile();
-				document.getElementById("editor").innerText= await file.text();;
+				document.getElementById("editor").innerText = await file.text();
 
 				// const out = {};
 				// const dirHandle = await showDirectoryPicker();
@@ -162,15 +197,14 @@ export default class Debugger {
 			}
 
 			case "asm-asm": {
-				const src= document.getElementById("editor").innerText;
-				const opts= {
-					readFile: () => ({path: "", content: src}),
+				const src = document.getElementById("editor").innerText;
+				const opts = {
+					readFile: () => ({ path: "", content: src }),
 				};
 				try {
-					const obj= assemble("", opts);
+					const obj = assemble("", opts);
 					this.storeInMem(obj);
-				}
-				catch(e) {
+				} catch (e) {
 					console.log(e.message);
 				}
 				break;
@@ -181,6 +215,22 @@ export default class Debugger {
 				const file = await fileHandle.getFile();
 				this.vm.setDisk(0, new Uint8Array(await file.arrayBuffer()));
 				this.diskName.textContent = fileHandle.name;
+				break;
+			}
+
+			case "memEditor": {
+				if(this.mem)
+					break;
+				const widget= {
+					x:7,
+					y:6,
+					w:4,
+					h:4,
+					onClose: () => {this.mem= null},
+				};
+				const height= this.addWidget(this.grid, widget, "mem");
+				const memoryPanel = document.querySelector("#mem");
+				this.mem = new MemViewer(this, memoryPanel, Math.floor(height / 11));
 				break;
 			}
 
@@ -202,24 +252,23 @@ export default class Debugger {
 				break;
 
 			case "step_over":
-				this.vm.stepOver().then(()=>this.update());
+				this.vm.stepOver().then(() => this.update());
 				break;
 
 			case "step_into": {
 				performance.clearMarks();
 				performance.clearMeasures();
 				performance.mark("step");
-				this.vm.step().then(()=> {
+				this.vm.step().then(() => {
 					// performance.measure("STEP", "step");
 
 					this.update();
 
 					performance.measure("TOTAL", "step");
 
-					performance.getEntriesByType("measure")
-						.forEach(entry => console.log(`${entry.name}:${entry.duration}`));
-
-
+					for (const entry of performance.getEntriesByType("measure")) {
+						console.log(`${entry.name}:${entry.duration}`);
+					}
 				});
 				break;
 			}
@@ -238,65 +287,58 @@ export default class Debugger {
 	}
 
 	storeInMem(obj) {
-		let addr= obj.segments.CODE.start;
-		obj.obj.CODE.forEach((value) => {
-			this.memory[addr++]= value;
-		});
+		let addr = obj.segments.CODE.start;
+		for (const value of obj.obj.CODE) {
+			this.memory[addr++] = value;
+		}
 	}
 
 	updateBtns(isPaused) {
-		const btnList= this.uiroot.querySelectorAll(".btn");
-
-		btnList.forEach(btn => {
-				if(!isPaused) {
-					btn.classList.add("running");
-				}
-				else {
-					btn.classList.remove("running");
-				}
-			});
+		const btnList = document.querySelectorAll(".btn");
+		for (const btn of btnList) {
+			if (!isPaused) {
+				btn.classList.add("running");
+			} else {
+				btn.classList.remove("running");
+			}
+		}
 	}
 
 	onClickDisasm(e) {
-		const instructionID= e.target.parentElement.id;
-		const bank= parseInt(document.querySelector(`#${instructionID} .bank`).attributes["data-bank"]?.value, 16);
-		const addr= parseInt(document.querySelector(`#${instructionID} .addr`).attributes["data-addr"]?.value, 16);
+		const instructionID = e.target.parentElement.id;
+		const bank = Number.parseInt(document.querySelector(`#${instructionID} .bank`).attributes["data-bank"]?.value, 16);
+		const addr = Number.parseInt(document.querySelector(`#${instructionID} .addr`).attributes["data-addr"]?.value, 16);
 
-		if(!Number.isNaN(bank) && !Number.isNaN(addr))
-			this.toggleBreakpoint(bank*0x10000 + addr);
+		if (!Number.isNaN(bank) && !Number.isNaN(addr)) this.toggleBreakpoint(bank * 0x10000 + addr);
 	}
 
 	onClickRegister(e) {
-
-		if(e.target.className === "register") {
-			const value= prompt(`Hexa value for register ${e.target.id}`);
-			this.vm.updateCPUregister(e.target.id, parseInt(value, 16));
-		}
-		else {
-			const target= e.target.parentElement;
-			if(target.className !== "status")
-				return;
-			this.vm.updateCPUregister(target.id, 1-target.querySelector(".flag").innerText);
+		if (e.target.className === "register") {
+			const value = prompt(`Hexa value for register ${e.target.id}`);
+			this.vm.updateCPUregister(e.target.id, Number.parseInt(value, 16));
+		} else {
+			const target = e.target.parentElement;
+			if (target.className !== "status") return;
+			this.vm.updateCPUregister(target.id, 1 - target.querySelector(".flag").innerText);
 		}
 		this.update();
 	}
 
 	onClickBreakpoints(e) {
-		const bpIdx= e.target.parentElement.id;
-		switch(e.target.className) {
+		const bpIdx = e.target.parentElement.id;
+		switch (e.target.className) {
 			case "bpn": {
-				if(bpIdx < this.breakpoints.length) {
+				if (bpIdx < this.breakpoints.length) {
 					this.toggleBreakpoint(this.breakpoints[bpIdx]);
 				}
 				break;
 			}
 			case "bpa": {
-				const currentValue= bpIdx<this.breakpoints.length ? this.breakpoints[bpIdx] : null;
-				let value= prompt("Enter Breakpoint address", currentValue ? utils.hexword(currentValue) : "");
-				value= parseInt(value,16);
-				if(!isNaN(value) && value != currentValue) {
-					if(currentValue)
-						this.toggleBreakpoint(currentValue);
+				const currentValue = bpIdx < this.breakpoints.length ? this.breakpoints[bpIdx] : null;
+				let value = prompt("Enter Breakpoint address", currentValue ? utils.hexword(currentValue) : "");
+				value = Number.parseInt(value, 16);
+				if (!Number.isNaN(value) && value !== currentValue) {
+					if (currentValue) this.toggleBreakpoint(currentValue);
 					this.toggleBreakpoint(value);
 				}
 				break;
@@ -311,12 +353,12 @@ export default class Debugger {
 	}
 
 	toggleBreakpoint(addr) {
-		const idx= this.breakpoints.indexOf(addr);
-		if(idx>=0) {
-			this.vm.sendMessage("removeBP",{addr});
+		const idx = this.breakpoints.indexOf(addr);
+		if (idx >= 0) {
+			this.vm.sendMessage("removeBP", { addr });
 			this.breakpoints.splice(idx, 1);
 		} else {
-			this.vm.sendMessage("addBP",{addr});
+			this.vm.sendMessage("addBP", { addr });
 			this.breakpoints.push(addr);
 		}
 		localStorage.setItem(`${id}-bps`, JSON.stringify(this.breakpoints));
@@ -324,31 +366,32 @@ export default class Debugger {
 	}
 
 	stop() {
-		this.stopOnOpcode= 0;
+		this.stopOnOpcode = 0;
 		this.update();
 	}
 
-	async prevInstruction(cpuState, address) {
+	async prevInstruction(cpuState, addr) {
+		let address = addr;
 		address &= 0xffff;
-		let bestAddr= address - 1;
-		let bestScore= 0;
-		for (let startingPoint= address - 20; startingPoint !== address; startingPoint++) {
-			let score= 0;
-			let addr= startingPoint & 0xffff;
-			while (addr < address) {
-				const result= await this.disassembler.disassemble(this.mem.dumpMemBank, addr, cpuState);
+		let bestAddr = address - 1;
+		let bestScore = 0;
+		for (let startingPoint = address - 20; startingPoint !== address; startingPoint++) {
+			let score = 0;
+			let startAddr = startingPoint & 0xffff;
+			while (startAddr < address) {
+				const result = await this.disassembler.disassemble(this.mem.dumpMemBank, startAddr, cpuState);
 				if (result[0] === cpuState.PC) score += 10; // huge boost if this instruction was executed
 				if (result[0].match(commonInstructions) && !result[0].match(uncommonInstrucions)) {
 					score++;
 				}
 				if (result[1] === address) {
 					if (score > bestScore) {
-						bestScore= score;
-						bestAddr= addr;
+						bestScore = score;
+						bestAddr = startAddr;
 						break;
 					}
 				}
-				addr= result[1];
+				startAddr = result[1];
 			}
 		}
 		return bestAddr;
@@ -356,83 +399,77 @@ export default class Debugger {
 
 	// TODO : test syntax.js -> https://github.com/williamtroup/Syntax.js/tree/main
 	async updateDisasm(cpuState) {
-		const buildLine= ({lineID, addr, disasm, comment, selected}) => {
-			const bank= utils.hexbyte(this.mem.dumpMemBank);
-			addr= utils.hexword(addr);
+		const buildLine = ({ lineID, addr, disasm, comment, selected }) => {
+			const bank = utils.hexbyte(this.mem?.dumpMemBank);
+			addr = utils.hexword(addr);
 			return `
-				<div class="line ${selected?"selected":""}">
+				<div class="line ${selected ? "selected" : ""}">
 					<div class="instruction" id="inst${lineID}">
 						<div class="bank" data-bank="${bank}"></div>
 						<div class="addr" data-addr="${addr}"></div>
 						<div class="disasm">${disasm}</div>
-					</div>` +
-					(comment ?
-						`<div class="comment">${comment}</div>`
-						:
-						""
-					) +
-				"</div>";
+					</div>${comment ? `<div class="comment">${comment}</div>` : ""}</div>`;
 		};
 
-		this.vm.waitMessage("disasm", {bank: this.mem.dumpMemBank, addr: cpuState.PC, lineCount: DISASM_LINES_COUNT})
-			.then(lines => {
-				let disasmStr= "";
-				for(let lineIdx= 0; lineIdx<lines.length; lineIdx++) {
-					disasmStr+= buildLine(lines[lineIdx]);
+		this.vm
+			.waitMessage("disasm", { bank: this.mem?.dumpMemBank, addr: cpuState.PC, lineCount: this.disasmLinecount-1 })
+			.then((lines) => {
+				let disasmStr = "";
+				for (let lineIdx = 0; lineIdx < lines.length; lineIdx++) {
+					disasmStr += buildLine(lines[lineIdx]);
 				}
-				this.UIdisasm.innerHTML= disasmStr;
+				this.UIdisasm.innerHTML = disasmStr;
 			});
-
 	}
 
 	updateStack(cpuState) {
-		let dumpStr= "";
-		const stackAddr= (cpuState.SP-2) & 0xff;
-		const currentSP= 0x100 | cpuState.SP;
-		for(let line= 0; line<10; line++) {
-			const addr= 0x100 | (stackAddr + line);
-			dumpStr+= `<div class="${addr === currentSP?"selected":""}">
+		let dumpStr = "";
+		const stackAddr = cpuState.SP - this.stackLinecount / 2;
+		const currentSP = 0x100 | cpuState.SP;
+		for (let line = 0; line < this.stackLinecount; line++) {
+			const addr = 0x100 | (stackAddr + line);
+			dumpStr += `<div class="${addr === currentSP ? "selected" : ""}">
 							${utils.hexword(addr)}: ${utils.hexbyte(this.memory[addr])}
 						</div>`;
 		}
-		this.UIstack.innerHTML= dumpStr;
+		this.UIstack.innerHTML = dumpStr;
 	}
 
 	updateBreakpoints() {
-		let dumpStr= "";
-		for(let line= 0; line<10; line++) {
-			dumpStr+= `<div id="${line}">
+		let dumpStr = "";
+		for (let line = 0; line < 10; line++) {
+			dumpStr += `<div id="${line}">
 							<div class="bpn">BP${line}</div>
-							<div class="bpa">${line< this.breakpoints.length ? utils.hexword(this.breakpoints[line]) : ""}</div>
+							<div class="bpa">${line < this.breakpoints.length ? utils.hexword(this.breakpoints[line]) : ""}</div>
 						</div>`;
 		}
-		this.UIbps.innerHTML= dumpStr;
+		this.UIbps.innerHTML = dumpStr;
 	}
 
 	updateRegisters(cpuState) {
-		this.registers.pc.innerHTML= utils.hexword(cpuState.PC);
-		this.registers.a.innerHTML= utils.hexbyte(cpuState.A);
-		this.registers.x.innerHTML= utils.hexbyte(cpuState.X);
-		this.registers.y.innerHTML= utils.hexbyte(cpuState.Y);
-		this.registers.sp.innerHTML= utils.hexword(0x100 | cpuState.SP);
+		this.registers.pc.innerHTML = utils.hexword(cpuState.PC);
+		this.registers.a.innerHTML = utils.hexbyte(cpuState.A);
+		this.registers.x.innerHTML = utils.hexbyte(cpuState.X);
+		this.registers.y.innerHTML = utils.hexbyte(cpuState.Y);
+		this.registers.sp.innerHTML = utils.hexword(0x100 | cpuState.SP);
 		// this.registers.p.innerHTML= utils.hexbyte(cpuState.p.asByte());
 
-		this.uiroot.querySelectorAll(".p.register .status").forEach((el) => {
-			el.querySelector(".flag").innerHTML= cpuState.P[el.id] ? 1 : 0;
-		});
+		for (const el of document.querySelectorAll(".p.register .status")) {
+			el.querySelector(".flag").innerHTML = cpuState.P[el.id] ? 1 : 0;
+		}
 	}
 
 	async update() {
+		this.vm.getCPUstate().then((cpuState) => {
+			this.lastCPUstate= cpuState;
 
-		this.vm.getCPUstate().then(cpuState => {
 			this.updateStack(cpuState);
 			this.updateRegisters(cpuState);
 			this.updateDisasm(cpuState);
 		});
 		this.updateBreakpoints();
-		this.mem.update();
+		this.mem?.update();
 
 		console.flush ? console.flush() : console.clear();
 	}
-
 }
