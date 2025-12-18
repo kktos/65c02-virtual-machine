fc4ca5c04c79b97b26e9ca0c37ea370378503da2
kktos <tm.barral@gmail.com>
2025-12-18 19:04:42 +0100

    refactor: title smaller

--- packages/vm6502/src/debugger/DebuggerPanelTitle.vue
+++ packages/vm6502/src/debugger/DebuggerPanelTitle.vue
@@ -1,5 +1,5 @@
 <template>
-	<h2 class="text-sm font-semibold text-cyan-400 border-b border-gray-700/50 pb-2 mb-3 uppercase tracking-wider">
+	<h2 class="text-xs font-semibold text-cyan-400 border-b border-gray-700/50 pb-2 mb-3 uppercase tracking-wider">
 		{{ title }}
 	</h2>
 </template>
