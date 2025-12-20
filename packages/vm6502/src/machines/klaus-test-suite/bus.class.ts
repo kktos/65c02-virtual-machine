3a7a9242e5d5134f599022373df782113bf87774
kktos <tm.barral@gmail.com>
2025-12-20 09:26:27 +0100

    refactor:cleanup

--- packages/vm6502/src/machines/klaus-test-suite/bus.class.ts
+++ packages/vm6502/src/machines/klaus-test-suite/bus.class.ts
@@ -7,15 +7,15 @@ export class Bus implements IBus {
 		this.memory = memory;
 	}
 
-	read(address: number, isOpcodeFetch?: boolean): number {
+	read(address: number, _isOpcodeFetch?: boolean): number {
 		if (address < 0 || address >= this.memory.length) {
 			throw new Error(`Memory read out of bounds: 0x${address.toString(16)}`);
 		}
 
-		if (!isOpcodeFetch)
-			console.log(
-				`read ${address.toString(16).padStart(4, "0")}:${(this.memory[address] ?? 0).toString(16).padStart(2, "0")}`,
-			);
+		// if (!isOpcodeFetch)
+		// 	console.log(
+		// 		`read ${address.toString(16).padStart(4, "0")}:${(this.memory[address] ?? 0).toString(16).padStart(2, "0")}`,
+		// 	);
 
 		return this.memory[address] ?? 0;
 	}
