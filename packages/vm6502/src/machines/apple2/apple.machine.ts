a649b140cccafaed4715e69816b0dfb892834548
kktos <tm.barral@gmail.com>
2025-12-25 18:07:32 +0100

    feat: set debugoptions for apple

--- packages/vm6502/src/machines/apple2/apple.machine.ts
+++ packages/vm6502/src/machines/apple2/apple.machine.ts
@@ -67,4 +67,51 @@ export const apple2e: MachineConfig = {
 		path: "apple2/apple.video",
 	},
 	disk: { enabled: true, name: "SPDisk" },
+	debugOptions: [
+		{
+			id: "lcView",
+			label: "LC ROM",
+			type: "select",
+			category: "memory",
+			options: [
+				{ label: "Auto", value: "AUTO" },
+				{ label: "ROM", value: "ROM" },
+				{ label: "LC Bank 1", value: "BANK1" },
+				{ label: "LC Bank 2", value: "BANK2" },
+			],
+		},
+		{
+			id: "cxView",
+			label: "Cx ROM",
+			type: "select",
+			category: "memory",
+			options: [
+				{ label: "Auto", value: "AUTO" },
+				{ label: "Internal", value: "INT" },
+				{ label: "Slots", value: "SLOT" },
+			],
+		},
+		{
+			id: "videoMode",
+			label: "Video Mode",
+			type: "select",
+			category: "video",
+			options: [
+				{ label: "Auto", value: "AUTO" },
+				{ label: "Text", value: "TEXT" },
+				{ label: "HGR", value: "HGR" },
+			],
+		},
+		{
+			id: "videoPage",
+			label: "Video Page",
+			type: "select",
+			category: "video",
+			options: [
+				{ label: "Auto", value: "AUTO" },
+				{ label: "Page 1", value: "PAGE1" },
+				{ label: "Page 2", value: "PAGE2" },
+			],
+		},
+	],
 };
