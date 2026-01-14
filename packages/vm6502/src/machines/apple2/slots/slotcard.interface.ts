c869ee053ba06cc2b29f893c4f5e9762acc37104
kktos <tm.barral@gmail.com>
2026-01-14 10:57:57 +0100

    refactor: card intf

--- packages/vm6502/src/machines/apple2/slots/slotcard.interface.ts
+++ packages/vm6502/src/machines/apple2/slots/slotcard.interface.ts
@@ -1,11 +1,16 @@
 import type { IBus } from "@/virtualmachine/cpu/bus.interface";
 
 export interface ISlotCard {
+	slot: number;
+
 	readRom(offset: number): number;
+	writeRom?(offset: number, value: number): void;
 	readIo(offset: number): number;
 	writeIo(offset: number, value: number): void;
 	readExpansion(offset: number): number;
+	writeExpansion?(offset: number, value: number): void;
 	setRegisters?(view: DataView): void;
 	setBus?(bus: IBus): void;
 	insertMedia?(data: Uint8Array): void;
+	tick?(cycles: number): Float32Array[];
 }
