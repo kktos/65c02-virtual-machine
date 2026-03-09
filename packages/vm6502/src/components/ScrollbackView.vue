3d34a2876e63bd5ce2b94e69c17bb9da8a45833e
kktos <tm.barral@gmail.com>
2026-03-09 12:21:42 +0100

    refactor: empty lines have height in console

--- packages/vm6502/src/components/ScrollbackView.vue
+++ packages/vm6502/src/components/ScrollbackView.vue
@@ -2,8 +2,8 @@
 	<div class="h-full flex flex-col">
 		<div class="flex-1 overflow-y-auto overflow-x-hidden p-2 flex flex-col" ref="containerRef">
 			<div class="mt-auto space-y-0.5">
-				<div v-for="(line, i) in logs" :key="i" :class="line.color" class="whitespace-pre">
-					{{ line.text }}
+				<div v-for="(line, i) in logs" :key="i" :class="line.color" class="whitespace-pre-wrap">
+					{{ line.text || "\u00A0" }}
 				</div>
 				<div ref="logEndRef"></div>
 			</div>
