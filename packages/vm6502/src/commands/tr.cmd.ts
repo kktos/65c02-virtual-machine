02898bfeb4b5c316876028d3250568715d157e06
kktos <tm.barral@gmail.com>
2026-03-19 19:38:58 +0100

    feat: cmd tr

---
+++ packages/vm6502/src/commands/tr.cmd.ts
@@ -0,0 +1,33 @@
+import type { Command, ParamListItemIdentifier } from "@/types/command";
+
+export const transformCmd: Command = {
+	description: "Tranform a stream. Usage: `content_producer` |> TR type",
+	paramDef: ["name", "string"],
+	group: "Streams",
+	fn: async ({ params }) => {
+		const type = params[0] as ParamListItemIdentifier;
+		const content = params[1] as string;
+
+		switch (type.text.toUpperCase()) {
+			// case "JSON":
+			case "TEXT":
+				return convertToText(content);
+
+			default:
+				throw new Error("Uknown transform type");
+		}
+	},
+};
+
+function convertToText(content: string): string {
+	return content
+		.replaceAll("<br/>", "\n")
+		.replaceAll("&nbsp;", " ")
+		.replaceAll("\\-", "-")
+		.replaceAll("\\_", "_")
+		.replaceAll("\\`", "`")
+		.replaceAll("\\~", "~")
+		.replaceAll("\\#", "#")
+		.replaceAll("\\>", ">")
+		.replaceAll("\\*", "*");
+}
