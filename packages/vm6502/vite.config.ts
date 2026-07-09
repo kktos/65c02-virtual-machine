f13dca3dbfab1435dded8fe5b480a83edb398250
kktos <tm.barral@gmail.com>
2026-07-09 18:53:26 +0200

    fix: vite 8 change the way the worker is created

--- packages/vm6502/vite.config.ts
+++ packages/vm6502/vite.config.ts
@@ -2,11 +2,24 @@ import path from "node:path";
 import tailwindcss from "@tailwindcss/vite";
 import vue from "@vitejs/plugin-vue";
 import { defineConfig } from "vite";
+// import { createRequire } from "node:module";
+
+// const require = createRequire(import.meta.url);
 
 // https://vite.dev/config/
 export default defineConfig({
 	assetsInclude: ["**/*.woff", "**/*.woff2", "**/*.ttf", "**/*.otf"],
-	plugins: [vue(), tailwindcss()],
+	plugins: [
+		vue({
+			// script: {
+			// 	fs: require("node:fs"),
+			// },
+		}),
+		tailwindcss(),
+	],
+	worker: {
+		format: "es",
+	},
 	resolve: {
 		alias: {
 			"@": path.resolve(__dirname, "./src"),
