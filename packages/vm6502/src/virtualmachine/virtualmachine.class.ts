30c5c8ae61412a097b712a6fa46e4af31cbc7fdd
kktos <tm.barral@gmail.com>
2026-07-10 20:12:41 +0200

    fix:cleanup types

--- packages/vm6502/src/virtualmachine/virtualmachine.class.ts
+++ packages/vm6502/src/virtualmachine/virtualmachine.class.ts
@@ -1,7 +1,7 @@
 import { ref } from "vue";
 import type { Dict } from "@/types/dict.type";
 import type { MachineConfig } from "@/types/machine.interface";
-import type { IBus, MachineStateSpec } from "@/types/bus.interface";
+import type { IBus } from "@/types/bus.interface";
 import {
 	FLAG_B_MASK,
 	FLAG_C_MASK,
@@ -616,7 +616,7 @@ export class VirtualMachine {
 		return this.machineConfig.debug?.options ?? [];
 	}
 
-	public getMachineStateSpecs(): MachineStateSpec[] {
+	public getMachineStateSpecs() {
 		return this.bus?.getMachineStateSpecs ? this.bus.getMachineStateSpecs() : [];
 	}
 
